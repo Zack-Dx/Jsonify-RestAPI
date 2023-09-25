8ee7513727682200f54c9871468587addbc503a0
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2023-09-25 16:03:09 +0530

    Logger setup done.

--- src/server.js
+++ src/server.js
@@ -1,16 +1,19 @@
 import app from './app.js';
-import Config from './config/conf.js';
+import Config from './config/index.js';
+import logger from './config/logger.js';
 
 const { PORT } = Config;
 
 function startServer () {
     try {
         app.listen(PORT, () => {
-            console.log(`Server is listening on ${PORT}`);
+            logger.info(`Listening on port ${PORT}`);
         });
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
+    } catch (err) {
+        if (err instanceof Error) {
+            logger.error(err.message);
+            setTimeout(() => process.exit(1), 1000);
+        }
     }
 }
 
