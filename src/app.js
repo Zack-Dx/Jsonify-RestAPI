837c1b354a6eae634e60213bfb1a4e555282513d
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2023-09-27 15:15:43 +0530

    Rate Limiter Middleware added.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import router from './routes/User_Routes.js';
 import { errorHandler } from './middleware/error_middleware.js';
+import rateLimiter from './config/limiter.js';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(router);
 app.use(errorHandler);
 
