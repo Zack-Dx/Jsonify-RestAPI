8acee63db2335712a3084882e44dbcf7ecb86ed9
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2023-09-29 00:11:00 +0530

    daily file rotater added.

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,21 +1,30 @@
 import Config from './index.js';
 import winston from 'winston';
+import 'winston-daily-rotate-file';
 
-const logger = new winston.createLogger({
+const logger = winston.createLogger({
     level: 'info',
     defaultMeta: {
         appName: Config.APP_NAME,
     },
     transports: [
-        new winston.transports.File({
+        new winston.transports.DailyRotateFile({
             dirname: 'logs',
             filename: 'combined.log',
+            datePattern: 'YYYY-MM-DD-HH',
+            zippedArchive: true,
+            maxSize: '10m',
+            maxFiles: '7d',
             level: 'info',
             silent: false,
         }),
-        new winston.transports.File({
+        new winston.transports.DailyRotateFile({
             dirname: 'logs',
             filename: 'error.log',
+            datePattern: 'YYYY-MM-DD-HH',
+            zippedArchive: true,
+            maxSize: '10m',
+            maxFiles: '7d',
             level: 'error',
             silent: false,
         }),
