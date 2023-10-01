69ff3a1a578a60137a3727020bb1825385bb1f3f
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2023-10-01 15:59:58 +0530

    proxy fixes.

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import { rateLimiter } from "./config/limiter/index.js"
 const app = express()
 
 // Global Middlewares
-
+app.set("trust proxy", true)
 app.use(
   cors({
     origin: Config.CORS_ORIGIN,
